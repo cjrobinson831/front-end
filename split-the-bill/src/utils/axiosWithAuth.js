419f4614a49595dbe671289392fe59e3a2ddec83
VulpineReynard <milare8@gmail.com>
2019-08-26 18:21:22 -0400

    minor adjustments

--- split-the-bill/src/utils/axiosWithAuth.js
+++ split-the-bill/src/utils/axiosWithAuth.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export const axiosWithAuth = () => {
+  // set the token from local storage to a variable
   const token = localStorage.getItem("token");
 
+  // return the token as the header prop 'Authorization' 
+  // to be used to authenticate on API requests
   return axios.create({
     headers: {
       Authorization: token
