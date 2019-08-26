047f7c88c9f8f36e928ebd4b77cc313471ea8895
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-08-26 14:09:37 -0400

    created Dashboard function and imported libraries

---
+++ split-the-bill/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import {Route} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Container, Segment, Header, Card, Button, Icon, Modal } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.css'; 
+import 'semantic-ui-css/semantic.min.css'; 
+
+export default function Dashboard (props) {
+
+    
+
+
+}
+
