e9933c6806147d00b19f4fbccb42ec670c12feb2
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-08-26 15:07:27 -0400

    completed draft layout 1 of dashboard

--- split-the-bill/src/components/Dashboard.js
+++ split-the-bill/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import {Route} from "react-router-dom";
 import React, { useState, useEffect } from 'react';
-import { Container, Segment, Header, Card, Button, Icon, Modal } from 'semantic-ui-react';
+import { Container, Segment, Header, Card, Button, Icon, Modal, Menu } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.css'; 
 import 'semantic-ui-css/semantic.min.css'; 
 import "../styling/App.css";
@@ -13,39 +13,41 @@ export default function Dashboard (props) {
     
             <div className = "navbar">
 
-                    <h1>SplitProof</h1>                      
+                    <h1>SplitProof</h1>  
+
+                     <button> Log Out </button>                       
     
             </div>
 
-            <header>
-                    <h1>Dashboard</h1>
-    
-                    <button> Add An Expense </button>        
-    
-            </header>
-    
-            <div className = "bills-list-div">
-    
-                <Modal
-                trigger={
-                    <Button id='add' icon >
-                    <Icon name='add circle' />
-                    </Button>
-                } closeIcon>
-    
-                <Modal.Header>Add a Bill</Modal.Header>
-    
+            <div className = "dashboard-div">
+
+                <div className = "dashboard-header-div">
+                        <h1>Welcome "User" </h1>
+        
+                        <button> Add An Expense </button>        
+        
+                </div>
+
+                <ul className="totals-header">
+                    <li>                    
+                        You Need to Pay Your Friends
+                    </li>
+
+                    <li>                    
+                        Your Friends Owe You
+                    </li>
+                </ul>
                 
-               
-                </Modal>
-    
-              
-    
-    
-                
-            </div>
-    
-        </div>
+                <div className = "bills-list-div">
+        
+                    
+        
+                    
+                </div>
+
+            </div> {/*end dashboard div */}
+        
+        </div> /*end container div */
     
         );
     
