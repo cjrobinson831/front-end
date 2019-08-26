f13c23fec1af6d4b347101ad4076139248a10035
VulpineReynard <milare8@gmail.com>
2019-08-26 15:18:55 -0400

    wip: login form - structured, need to setup state and send it to login endpoint

--- split-the-bill/src/App.js
+++ split-the-bill/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginSignUpScreen from './components/LoginSignUpScreen';
+import PrivateRoute from './components/PrivateRoute';
+import Header from './components/Header';
 
 function App() {
   return (
+    // app wrapped in a router, starts by default with login page
+    // will route users to dashboard once signed in or registered
     <Router>
       <div className="App">
-      <Switch>
-        <Route exact path="/" component={LoginSignUpScreen}/>
-      </Switch>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={LoginSignUpScreen}/>
+          <PrivateRoute path="/protected"/>
+        </Switch>
       </div>
     </Router>
   );
--- split-the-bill/src/components/LoginSignUpScreen.js
+++ split-the-bill/src/components/LoginSignUpScreen.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const LoginSignupScreen = () => {
   return (
-    <div>
+    <div className="login-screen">
+      <div className="login-form-container">
+        <h2 className="login-form-title">Login Below</h2>
+        <form className="login-form">
+          <input 
+          className="input-email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          />
+          <input 
+          className="input-password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          />
+          <button>Login</button>
+        </form>
+        <div className="register-description">
 
+        </div>
+      </div>
     </div>
   );
 }
